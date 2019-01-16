fead9eaa9e471052187506d18360d168e1aab6a9
EJaona <ejaona@gmail.com>
2019-01-16 05:31:59 -0600

    turned logo into an anchor tag that just refreshes the page to leave filtered search

--- instagram/src/Components/SearchBar/HeaderLogo.js
+++ instagram/src/Components/SearchBar/HeaderLogo.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const HeaderLogo = () => (
-  <div className="logo">
+  <a
+    href="" //Currently refreshes page. Will adjust the rout to "Home" when we learn about routing
+    className="logo"
+    style={{ color: "black", textDecoration: "none" }}
+  >
     <i className="fab fa-instagram fa-lg fa-2x" /> <h1>Instagram</h1>
-  </div>
+  </a>
 );
 
 export default HeaderLogo;
