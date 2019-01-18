d9d31d8f0b2de3664a31e5471d8817eb46988eff
EJaona <ejaona@gmail.com>
2019-01-18 05:17:16 -0600

    Updates

---
+++ precache-manifest.53b9a13ca205fc28844a32f14cd3bc7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fd62b77bd3bdb7b11ec",
+    "url": "/Fakestagram/static/js/runtime~main.5fd62b77.js"
+  },
+  {
+    "revision": "5a19d79d3517de6f47d9",
+    "url": "/Fakestagram/static/js/main.5a19d79d.chunk.js"
+  },
+  {
+    "revision": "2ca95ee63f7324bb4622",
+    "url": "/Fakestagram/static/js/1.2ca95ee6.chunk.js"
+  },
+  {
+    "revision": "5a19d79d3517de6f47d9",
+    "url": "/Fakestagram/static/css/main.c2a3484e.chunk.css"
+  },
+  {
+    "revision": "f7ae728f0306b9053cb02485ef6339b7",
+    "url": "/Fakestagram/index.html"
+  }
+];
